9697dba3d7a6a97f7f213722dd9a5af9a5d16dd6
JuanGarciaMontero <108418765+JuanGarciaMontero@users.noreply.github.com>
2022-11-03 22:10:59 +0100

    Update HelloController.java
    Prueba cambios desde github

--- src/main/java/com/example/springdeploy/HelloController.java
+++ src/main/java/com/example/springdeploy/HelloController.java
@@ -36,7 +36,7 @@ public class HelloController {
                     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
                   </head>
                   <body>
-                    <h1>Hola mundo desde SpringBoot!</h1>
+                    <h1>Hola mundo desde SpringBoot! Soy Juan.</h1>
                     <a class="btn btn-primary" href="https://www.google.com">Google</a>
                     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
                   </body>
